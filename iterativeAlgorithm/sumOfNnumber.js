2e36596720f513f2435930446d1bc26a7878ff31
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-05-19 19:11:37 +0530

    sum of number of elements in array

---
+++ iterativeAlgorithm/sumOfNnumber.js
@@ -0,0 +1,17 @@
+//for loop
+let arr = [1,2,3,4,5]
+let sum = 0
+for(let i = 0 ; i < arr.length ; i++){
+  sum += arr[i]
+}
+console.log(sum);
+
+//while loop 
+let arr2 = [10,20,30,40,50];
+let sum2 = 0
+let i = 0 ; 
+while(i < arr2.length){
+  sum2 += arr2[i]
+  i++
+}
+console.log(sum2);
